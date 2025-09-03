237e920d7bfd1779df8bd807750d8f618ba24fc1
Summer Riche <sriche9@lsu.edu>
2025-09-03 14:03:37 -0500

    finishedellipse

---
+++ sketch.js
@@ -0,0 +1,53 @@
+function setup (){
+    createCanvas(400, 400)
+
+}
+
+function draw (){
+    background(200);
+
+    fill ("salmon")
+    ellipse (200, 200, 570, 570);
+    ellipse (200, 200, 560, 560);
+    ellipse (200, 200, 500, 500);
+    ellipse (200, 200, 450, 450);
+    ellipse (200, 200, 410, 410);
+    ellipse (200, 200, 375, 375);
+    ellipse (200, 200, 345, 345);
+    ellipse (200, 200, 318, 318);
+    ellipse (200, 200, 293, 293);
+    ellipse (200, 200, 270, 270);
+    ellipse (200, 200, 250, 250);
+    ellipse (200, 200, 233, 233);
+    ellipse (200, 200, 219, 219);
+    ellipse (200, 200, 207, 207);
+    ellipse (200, 200, 196, 196);
+    ellipse (200, 200, 185, 185);
+    ellipse (200, 200, 174, 174);
+    ellipse (200, 200, 163, 163);
+    ellipse (200, 200, 152, 152);
+    ellipse (200, 200, 141, 141);
+    ellipse (200, 200, 130, 130);
+    ellipse (200, 200, 120, 120);
+    ellipse (200, 200, 110, 110);
+    ellipse (200, 200, 101, 101);
+    ellipse (200, 200, 93, 93);
+    ellipse (200, 200, 86, 86);
+    ellipse (200, 200, 80, 80);
+    ellipse (200, 200, 75, 75);
+    ellipse (200, 200, 70, 70);
+    ellipse (200, 200, 65, 65);
+    ellipse (200, 200, 60, 60);
+    ellipse (200, 200, 55, 55);
+    ellipse (200, 200, 50, 50);
+    ellipse (200, 200, 45, 45);
+    ellipse (200, 200, 40, 40);
+    ellipse (200, 200, 35, 35);
+    ellipse (200, 200, 30, 30);
+    ellipse (200, 200, 25, 25);
+    ellipse (200, 200, 20, 20);
+    ellipse (200, 200, 15, 15);
+    ellipse (200, 200, 10, 10);
+    ellipse (200, 200, 5, 5);
+
+}
